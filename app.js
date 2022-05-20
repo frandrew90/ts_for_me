7406952067eb81718e1710f21567fcf6c75e34fd
frandrew90 <frandrewjs@gmail.com>
2022-05-20 16:09:36 +0300

    made the first app

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-function sum(num1, num2) {
+var button = document.getElementById("btn");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+function add(num1, num2) {
     return num1 + num2;
 }
-console.log(sum(1, 1));
+// console.log(button);
+button.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value));
+});
--- app.ts
+++ app.ts
@@ -1,5 +1,13 @@
-function sum(num1: number, num2: number) {
+const button = document.getElementById("btn")! as HTMLButtonElement;
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-console.log(sum(1, 1));
+// console.log(button);
+
+button.addEventListener("click", function () {
+  console.log(add(+input1.value, +input2.value));
+});
